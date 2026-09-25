84c53d61ff800d71febf2c993566b5639828b3ef
marinabaravelli-spec <marina.baravelli@hubfuse.com.br>
2026-09-25 13:06:04 -0300

    atualização

--- src/components/home/Clients.tsx
+++ src/components/home/Clients.tsx
@@ -4,11 +4,11 @@ import Image from 'next/image';
 import styles from './Clients.module.css';
 
 const clients = [
-  { id: 'abc-founders', name: 'ABC Founders', src: '/images/clients/abc-founders.png' },
-  { id: 'clube-executivos', name: 'Clube dos Executivos', src: '/images/clients/clube-executivos.png' },
-  { id: 'vereda', name: 'Vereda', src: '/images/clients/vereda.png' },
   { id: 'saint-gobain', name: 'Saint-Gobain', src: '/images/clients/saint-gobain.png' },
   { id: 'token-nation', name: 'Token Nation', src: '/images/clients/token-nation.png' },
+  { id: 'vereda', name: 'Vereda', src: '/images/clients/vereda.png' },
+  { id: 'abc-founders', name: 'ABC Founders', src: '/images/clients/abc-founders.png' },
+  { id: 'clube-executivos', name: 'Clube dos Executivos', src: '/images/clients/clube-executivos.png' },
 ];
 
 export function Clients() {
